f0d7f4a9a45fe8b3f4a2480768aad588e891a64f
phwt.smwt <phwt.smwt@gmail.com>
2021-02-15 11:32:30 +0700

    Simple route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./screens/Home";
+import Launches from "./screens/Launches";
+import Rockets from "./screens/Rockets";
 
-function App() {
-  return <Home />;
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rockets">Rockets</Link>
+            </li>
+            <li>
+              <Link to="/launches">Launches</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/rockets">
+            <Rockets />
+          </Route>
+          <Route path="/launches">
+            <Launches />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
