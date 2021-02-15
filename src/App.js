c4ac6db131e79c8e070e9d0dba9c92014b579b13
phwt.smwt <phwt.smwt@gmail.com>
2021-02-15 12:06:11 +0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 import Home from "./screens/Home";
 import Launches from "./screens/Launches";
 import Rockets from "./screens/Rockets";
@@ -8,21 +14,26 @@ import Rockets from "./screens/Rockets";
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/rockets">Rockets</Link>
-            </li>
-            <li>
-              <Link to="/launches">Launches</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <Navbar bg="light" expand="lg" className="mb-4">
+        <LinkContainer to="/">
+          <Navbar.Brand>SpaceX</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer exact to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/rockets">
+              <Nav.Link>Rockets</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/launches">
+              <Nav.Link>Launches</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
       <Container>
         <Switch>
           <Route exact path="/">
