0ffa421d4536fd03d1199f46cd40b453d963ea1b
phwt.smwt <phwt.smwt@gmail.com>
2021-02-15 12:00:07 +0700

    Simple data fetching and display

--- src/screens/Rockets.js
+++ src/screens/Rockets.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Badge, Card } from "react-bootstrap";
+
+const RocketCard = ({ rocket }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{rocket.rocket_name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          <Badge pill variant={rocket.active ? "success" : "danger"}>
+            {rocket.active ? "Active" : "Inactive"}
+          </Badge>
+        </Card.Subtitle>
+        <Card.Text>{rocket.description}</Card.Text>
+        <Card.Link href={rocket.wikipedia}>Wikipedia</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+};
 
 const Rockets = () => {
-  return <p>Rockets</p>;
+  const [rockets, setRockets] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.spacexdata.com/v3/rockets");
+      setRockets(await response.json());
+    })();
+  }, []);
+
+  return (
+    <p>
+      {rockets.map((rocket) => (
+        <RocketCard key={rocket.id} rocket={rocket} />
+      ))}
+    </p>
+  );
 };
 
 export default Rockets;
