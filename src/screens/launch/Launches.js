8a00db89043bc3ded623d8cd5892094e08c8f04b
phwt.smwt <phwt.smwt@gmail.com>
2021-02-18 23:37:24 +0700

    Remove moment usage

--- src/screens/launch/Launches.js
+++ src/screens/launch/Launches.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import moment from "moment";
 
 const Launches = () => {
   const [state, setState] = useState([]);
@@ -14,14 +13,17 @@ const Launches = () => {
       setState(data);
     })();
   }, []);
-  const launchItems = state.map((state) => (
-    <>
-      <h4>{state.flight_number}</h4>
-      <p>{state.mission_name}</p>
-      <p>{state.launch_year}</p>
-      <p>{moment.unix(state.launch_date_unix).format("MM/DD/YYYY")}</p>
-    </>
-  ));
+  const launchItems = state.map((state) => {
+    const launchDate = new Date(state.launch_date_unix * 1000);
+    return (
+      <>
+        <h4>{state.flight_number}</h4>
+        <p>{state.mission_name}</p>
+        <p>{state.launch_year}</p>
+        <p>{launchDate.toString()}</p>
+      </>
+    );
+  });
   return (
     <>
       <h1>Launches</h1>
