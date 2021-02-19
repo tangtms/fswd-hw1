54adb41846aa8585443d80849b08de504074ff20
tangtms <tang.tms@outlook.com>
2021-02-20 01:34:20 +0700

    Update Launches.js

--- src/screens/launch/Launches.js
+++ src/screens/launch/Launches.js
@@ -4,20 +4,30 @@ import { Link } from "react-router-dom";
 
 const Launches = () => {
   const [state, setState] = useState([]);
-  const [api, setApi] = useState(
-    "https://api.spacexdata.com/v3/launches?limit=6"
-  );
+  const [year, setYear] = useState("");
+  const [rocketName, setRocketName] = useState("");
+  const [launchSuccess, setLaunchSuccess] = useState("");
+
   useEffect(() => {
     (async () => {
-      const response = await fetch(api);
+      const response = await fetch(
+        "https://api.spacexdata.com/v3/launches?limit=6&launch_year=" +
+          year +
+          "&rocket_id=" +
+          rocketName +
+          "&launch_success=" +
+          launchSuccess
+      );
       const data = await response.json();
       console.log(data);
       setState(data);
     })();
-  }, []);
+  }, [year, rocketName, launchSuccess]);
 
-  // const filterYear(event) => {this.setApi("")};
-
+  // const filterYear = (e) => {
+  //   setYear(e.target.value);
+  // };
+  console.log(year);
   const launchItems = state.map((state) => {
     const launchDate = new Date(state.launch_date_unix * 1000);
     return (
@@ -35,23 +45,43 @@ const Launches = () => {
             </Link>
           </Card.Body>
         </Card>
-        {/* <h4>{state.flight_number}</h4>
-        <img src={state.links.mission_patch} />
-        <p>{state.mission_name}</p>
-        <p>{state.launch_year}</p>
-        <p>{launchDate.toString()}</p> */}
       </Col>
     );
   });
   return (
     <>
       <h1>Launches</h1>
-      {/* <select value={state.value}>
+      <select
+        onChange={(e) => {
+          setYear(e.target.value);
+        }}
+        value={year}
+      >
         <option value="2007">2007</option>
         <option value="2008">2008</option>
         <option value="2009">2009</option>
         <option value="2010">2010</option>
-      </select> */}
+      </select>
+      <select
+        onChange={(e) => {
+          setRocketName(e.target.value);
+        }}
+        value={rocketName}
+      >
+        <option value="falcon1">Falcon 1</option>
+        <option value="falcon9">Falcon 9</option>
+        <option value="falconheavy">Falcon Heavy</option>
+        <option value="starship">Starship</option>
+      </select>
+      <select
+        onChange={(e) => {
+          setLaunchSuccess(e.target.value);
+        }}
+        value={launchSuccess}
+      >
+        <option value="true">True</option>
+        <option value="false">False</option>
+      </select>
       <Row>{launchItems}</Row>
     </>
   );
