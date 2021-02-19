5a9c59324888b539bc805afeb7bf0bf8ddef2ee1
tangtms <tang.tms@outlook.com>
2021-02-20 01:00:01 +0700

    Update Launch.js

--- src/screens/launch/Launch.js
+++ src/screens/launch/Launch.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Launch = () => {
   let { launchId } = useParams();
+  const [launch, setLaunch] = useState();
 
-  return <p>Single Launch {launchId}</p>;
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://api.spacexdata.com/v3/launches/${launchId}`
+      );
+      setLaunch(await response.json());
+    })();
+  }, [launchId]);
+
+  return <p>Single Launchasdasdasd {launchId}</p>;
 };
 
 export default Launch;
