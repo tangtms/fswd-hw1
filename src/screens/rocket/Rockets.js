072a49de3a7625105ef5e840d11c8d9362a3f505
phwt.smwt <phwt.smwt@gmail.com>
2021-02-21 22:12:17 +0700

    Add rocket placeholder

--- src/screens/rocket/Rockets.js
+++ src/screens/rocket/Rockets.js
@@ -1,9 +1,49 @@
 import React, { useState, useEffect } from "react";
 import RocketCard from "../../components/RocketCard";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
+import ReactPlaceholder from "react-placeholder";
+
+const RocketRowPlaceholder = () => {
+  return (
+    <>
+      <Col
+        md="6"
+        className="d-flex justify-content-center align-middle mb-4"
+        style={{
+          alignItems: "center",
+        }}
+      >
+        <div
+          className="img-fluid rounded shadow"
+          style={{
+            width: 500,
+            height: 400,
+            backgroundColor: "#C4C4C4",
+          }}
+        />
+      </Col>
+      <Col md="6" className="mb-4">
+        <Card>
+          <Card.Body>
+            <ReactPlaceholder ready={false} type="textRow" className="mb-4" />
+            <ReactPlaceholder ready={false} type="text" rows={4} />
+            <hr />
+            <ReactPlaceholder
+              ready={false}
+              type="text"
+              rows={6}
+              className="mb-5"
+            />
+            <ReactPlaceholder ready={false} type="text" rows={3} />
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  );
+};
 
 const Rockets = () => {
-  const [rockets, setRockets] = useState();
+  const [rockets, setRockets] = useState([]);
 
   useEffect(() => {
     (async () => {
@@ -17,7 +57,15 @@ const Rockets = () => {
       <Col md="12">
         <h2>Rockets</h2>
       </Col>
-      {rockets &&
+
+      {rockets.length <= 0 && (
+        <>
+          <RocketRowPlaceholder />
+          <RocketRowPlaceholder />
+        </>
+      )}
+
+      {rockets.length > 0 &&
         rockets.map((rocket) => (
           <React.Fragment key={rocket.id}>
             <Col
