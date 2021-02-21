bd5b6034e3e7003c8c548f85fe115626a15988ef
phwt.smwt <phwt.smwt@gmail.com>
2021-02-21 22:14:18 +0700

    Disable dark theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css";
+// import "@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
