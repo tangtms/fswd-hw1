751ecb6d657a28dd9d4790642a723eaedd3a7150
phwt.smwt <phwt.smwt@gmail.com>
2021-02-22 10:25:23 +0700

    Improve contrast ratio

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./override.scss";
 // import "@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css";
 
 ReactDOM.render(
